2531f2577d0c7c734484a8522078966f931ec682
hooleymass <hockleymasese3@gmail.com>
2023-01-23 22:51:26 +0300

    initial commit

---
+++ printf/1-printf.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * _printf - function that produces output according to a format
+ * @format: character string
+ *
+ * Return: the number of characters printed (excluding the null byte)
+ */
+
+int _printf(const char *format, ...) {
+    va_list args;
+    int count = 0;
+
+    va_start(args, format);
+    for (int i = 0; format[i] != '\0'; i++) {
+        if (format[i] == '%') {
+            i++;
+            switch (format[i]) {
+                case 'c':
+                    putchar(va_arg(args, int));
+                    count++;
+                    break;
+                case 's':
+                    fputs(va_arg(args, char*), stdout);
+                    count += strlen(va_arg(args, char*));
+                    break;
+                case 'd':
+                case 'i':
+                    count += printf("%d", va_arg(args, int));
+                    break;
+                case '%':
+                    putchar('%');
+                    count++;
+                    break;
+                default:
+                    break;
+            }
+        } else {
+            putchar(format[i]);
+            count++;
+        }
+    }
+    va_end(args);
+    return count;
+}
+
